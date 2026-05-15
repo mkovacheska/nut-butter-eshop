f0079ea0524be0c68eec1446a5b1b6fc69c2c560
mihaelakovacheska <kovacheskamihaela@gmail.com>
2026-05-15 12:13:04 +0400

    Price logic changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -21,22 +21,22 @@ public class Main {
     @Bean
     CommandLineRunner start(ProductRepository repository) {
     return args -> {
-        Product p1 = new Product("Classic Almond Butter", "Creamy & salty almond butter", 12.00, "/images/almond-butter.jpeg");
+        Product p1 = new Product("Classic Almond Butter", "Creamy & salty almond butter", 10.00, "/images/almond-butter.jpeg");
         p1.getSizeModifiers().put("200g", 0.0);   
-        p1.getSizeModifiers().put("350g", 4.0);  
-        p1.getSizeModifiers().put("700g", 10.0);
+        p1.getSizeModifiers().put("350g", 1.7);  
+        p1.getSizeModifiers().put("700g", 13.4);
         repository.save(p1);
 
-        Product p2 = new Product("Crunchy Peanut Butter", "Roasted peanuts with a crunch", 8.00, "/images/peanut-butter.jpeg");
+        Product p2 = new Product("Crunchy Peanut Butter", "Roasted peanuts with a crunch", 5.00, "/images/peanut-butter.jpeg");
         p2.getSizeModifiers().put("200g", 0.0);
-        p2.getSizeModifiers().put("350g", 3.0);
-        p2.getSizeModifiers().put("700g", 7.0);
+        p2.getSizeModifiers().put("350g", 2.5);
+        p2.getSizeModifiers().put("700g", 5.0);
         repository.save(p2);
 
-        Product p3 = new Product("Cashew Cream", "Velvety smooth cashews butter", 15.00, "/images/hazelnut-butter.jpeg");
+        Product p3 = new Product("Creamy Hazelnut Butter", "Velvety smooth hazelnut butter", 10.00, "/images/hazelnut-butter.jpeg");
         p3.getSizeModifiers().put("200g", 0.0);
-        p3.getSizeModifiers().put("350g", 5.0);
-        p3.getSizeModifiers().put("700g", 12.0);
+        p3.getSizeModifiers().put("350g", 3.4);
+        p3.getSizeModifiers().put("700g", 18.4);
         repository.save(p3);
     };
 }
